b0ca31efbaca3046b11b16c5e6fc52ad6d254289
joao.campos <joao.campos@nerus.com.br>
2022-02-08 20:45:01 -0300

    Adiciona tabela/controller/model para pets

--- projeto back/infraestrutura/tabelas.js
+++ projeto back/infraestrutura/tabelas.js
@@ -3,6 +3,7 @@ class Tabelas {
     this.conexao = conexao
 
     this.criarAtendimentos()
+    this.criarPets()
   }
 
   criarAtendimentos() {
@@ -17,6 +18,22 @@ class Tabelas {
       }
     })
   }
+
+  criarPets() {
+
+    const sql = 
+      'CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, nome varchar(50), imagem varchar(200), PRIMARY KEY (id))'
+
+    this.conexao.query(sql, erro => {
+      if(erro) {
+        console.log(erro)
+      } else {
+        console.log('Tabela Pets foi criada com sucesso')
+      }
+    })
+  }
 }
+
+  
 
 module.exports = new Tabelas()
