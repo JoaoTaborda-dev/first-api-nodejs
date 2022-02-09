248278427932012ed771295fc68304283a86d408
joao.campos <joao.campos@nerus.com.br>
2022-02-08 22:37:21 -0300

    Adicionando fotos ao post de pets

--- projeto back/uploadDeArquivos.js
+++ projeto back/uploadDeArquivos.js
@@ -1,6 +1,10 @@
-const { log } = require('console');
 const fs = require('fs')
 
-fs.createReadStream('./assets/salsicha.jpg')
-    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
-    .on('finish', () => console.log('Imagem foi escrita com sucesso'))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}.jpg`
+
+    fs.createReadStream(caminho)
+    .pipe(fs.createWriteStream(novoCaminho))
+    .on('finish', () => callbackImagemCriada(novoCaminho))
+}
